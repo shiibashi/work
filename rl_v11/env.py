16f4b499c0b0b5b357a1b66467dba40970cef56d
systra <systra>
2019-08-15 23:11:54 +0900

    update v11

--- rl_v11/env.py
+++ rl_v11/env.py
@@ -1,6 +1,6 @@
+import cv2
 import numpy
 import pandas
-import random
 import os
 
 class Env(object):
@@ -10,78 +10,56 @@ class Env(object):
         self.columns = ["Open_MA_5_N", "Open_MA_25_N", "Open_MA_75_N", "Open_R_1"]
         self.target = "Profit"
         
-        lb = numpy.array([1, 1, -1])
-        ub = numpy.array([0, 0, 2])
-        self.observation_space = gym.spaces.Box(lb, ub, dtype=numpy.float32)
-        self.action_space = gym.spaces.Discrete(3) # 0: stay, 1: long, 2: short
+        self.action_size = 3
+        self.csv_size = (5, )
+        self.img_size = (180, 180, 3)
         
-        #self.seed()
         self.state = None
         
         self.time = None
-        self.code = None
-        self.code_data = None
-        self.power = None
-                
-    def seed(self, seed=None):
-        self.numpy_random, seed = gym.utils.seeding.np_random(seed)
-        return [seed]
+        self.capability = None
+        self.cut_line = None
         
     def step(self, action):
-        assert self.action_space.contains(action), "%r (%s) invalid"%(action, type(action))
-        reward = self.reward(action, self.time, self.code_data)
+        reward = self.reward(action, self.df, self.time)
         self.time += 1
-        self.cut_line += 0.001
-        done = self.done_flag(self.time, self.code_data)
-        next_obj = self.observation(self.time, self.code_data)
-        return next_obj, reward, done, {}
+        self.cut_line += 0.01
+        done = self.done_flag(self.df, self.time)
+        next_obs = self.observation(self.df, self.time)
+        return next_obs, reward, done, {}
         
     def reset(self):
-        if self.random_data:
-            filename = random.sample(self.filename_list, 1)[0]
-        else:
-            filename = "0500.csv"
-        code_data = self._load_code_data(filename)
-        #code_data = self.all_df.sample(300).reset_index(drop=True)
         self.time = 0
-        self.code = filename.split(".")[0]
-        self.code_data = code_data
-        self.power = 1
+        self.capability = 1
         self.cut_line = 1
-        return self.observation(self.time, self.code_data)
+        return self.observation(self.df, self.time)
 
-    def observation(self, time, df):
+    def observation(self, df, time):
         feature = numpy.array([df[col][time] for col in self.columns])
-        status = numpy.array([self.power - self.cut_line])
+        status = numpy.array([self.capability - self.cut_line])
         s = len(feature) + len(status)
-        obs = numpy.concatenate([feature, status]).reshape(1, s)
-        #return numpy.array([[df[col][time] for col in self.columns]])
+        csv_arr = numpy.concatenate([feature, status]).reshape(1, s)
+        img_arr = self._load_chart_img(self.df, time)
+        obs = (csv_arr, img_arr)
         return obs
         
-    def reward(self, action, time, df):
-        profit = self.code_data["profit"][time]
+    def reward(self, action, df, time):
+        profit = df[self.target][time]
         if action == 2: # short
             action = -1
         
-        if self.power - self.cut_line >= 0.5 and profit * action < 0:
-            self.power += profit * action * 10
-        else:
-            self.power += profit * action
-        return max(0, self.power - self.cut_line)
-
+        self.capability += profit * action
+        return max(0, self.capability - self.cut_line)
         
-    def done_flag(self, time, df):
-        if time < len(df) - 1 and self.power >= self.cut_line - 0.02:
+    def done_flag(self, df, time):
+        if time < len(df) - 1 and self.capability >= self.cut_line - 0.2:
             return False
         else:
             return True
         
-    def render(self, mode="human"):
-        pass
-        
-    def close(self):
-        pass
-        
-    def _load_code_data(self, filename):
-        df = pandas.read_csv("{}/{}".format(self.data_dir, filename))
-        return df
+    def _load_chart_img(self, df, time):
+        #timestamp = df["Timestamp"][time]
+        timestamp = 1333041840
+        img_arr = cv2.imread("dataset/img/{}.png".format(timestamp))
+        assert img_arr is not None
+        return numpy.array([img_arr]) # 1, 180, 180, 3
